fc8165cd9b35836aeecd90624167c7fa0332f72e
olexii.baguk@playtech.com <olexii.baguk@playtech.com>
2017-10-25 11:52:43 +0300

    add react

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,8 +1,26 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import store from './store'
 import {positionChange} from './actions'
+import * as synaptic from 'synaptic'
+import NetForm from './components/Netform'
 
-console.log('hellllo3')
-const arr = [1,2,3]
-const filteredArr = arr.filter(item=>item%2 === 0)
+console.log(synaptic)
 window.store = store
 window.positionChange = positionChange
+window.synaptic = synaptic
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+    	<p>first component</p>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('react-container')
+)
---
+++ frontend/js/components/NetForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class NetForm extends Component{
+	
+	render(){
+		return (
+			<div className={'Tile '+' pic'}>
+			</div>					
+		)	
+	}
+}
+
+export default NetForm
